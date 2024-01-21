e1220d86dbcdf4746aa4a5c0ecc9c85b0745337e
Никита Олегович Романенко <nikcom526@mail.ru>
2024-01-21 18:07:39 +0300

    Добавьте файлы проекта.

---
+++ zadanie 15/ISeries.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_15
+{
+    interface ISeries
+    {
+        void setStart(int x);
+        int getNext();
+        void reset();
+
+    }
+
+    class ArithProgression : ISeries
+    {
+        int step;
+        int startValue;
+        int currentValue;
+        public int getNext()
+        {
+            currentValue+=step;
+            return currentValue;
+        }
+        public void setStart(int x)
+        {
+            startValue = x;
+        }
+        public void reset()
+        {
+            currentValue = startValue;
+        }
+        public void setStep(int s)
+        {
+            step = s;
+            currentValue = startValue;
+        }
+    }
+    class GeomProgression
+    {
+        int step;
+        int startValue;
+        int currentValue;
+        public int getNext()
+        {
+            currentValue *= step;
+            return currentValue;
+        }
+        public void setStart(int x)
+        {
+            startValue = x;
+        }
+        public void reset()
+        {
+            currentValue = startValue;
+        }
+        public void setStep(int s)
+        {
+            step = s;
+            currentValue = startValue;
+        }
+    }
+}
---
+++ zadanie 15/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace zadanie_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArithProgression arith = new ArithProgression();
+            arith.setStart(10);
+            arith.setStep(2);
+            Console.WriteLine("арифметическая прогрессия:");
+            Console.WriteLine( arith.getNext());
+            Console.WriteLine(arith.getNext());
+            Console.WriteLine(arith.getNext());
+            arith.reset();
+            Console.WriteLine(arith.getNext());
+            Console.WriteLine(arith.getNext());
+            GeomProgression geom = new GeomProgression();
+            geom.setStart(6);
+            geom.setStep(2);
+            Console.WriteLine("геометрическая прогрессия:");
+            Console.WriteLine( geom.getNext());
+            Console.WriteLine(geom.getNext());
+            Console.WriteLine(geom.getNext());
+            geom.reset();
+            Console.WriteLine(geom.getNext());
+            Console.WriteLine(geom.getNext());
+            Console.ReadKey();
+        }
+    }
+}
